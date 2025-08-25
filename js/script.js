31d5c4d159ce4eced8a7e4a7697c69f56416c120
Serge <serge-mur@yandex.ru>
2025-08-25 16:28:34 +0300

    footer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    const headerBurger = document.querySelector('.header__burger');
+    const mobMenu = document.querySelector('.mob-menu');
+    headerBurger.addEventListener('click', () => {
+        headerBurger.classList.toggle('header__burger_open');
+        mobMenu.classList.toggle('mob-menu_open');
+        document.querySelector('body').classList.toggle('remove-scrolling');
+    });
+
     const questions = document.querySelectorAll('.faq__question');
     questions.forEach(question => {
         question.addEventListener('click', function() {
